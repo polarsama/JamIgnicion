75d3ba35495905d30c0ccaa03a0caeb38118fad4
David Rincon <andreska9506@gmail.com>
2024-04-16 13:03:49 -0500

    Update

--- Assets/Scripts/GenerarEnemigos.cs
+++ Assets/Scripts/GenerarEnemigos.cs
@@ -10,6 +10,8 @@ public class GenerarEnemigos : MonoBehaviour
 
     public Transform yGeneratorUp;
     public Transform yGeneratorDown;
+    public Transform xGeneratorLeft;
+    public Transform xGeneratorRight;
 
     public GameObject enemigos;
     public float TimeSpawn = 1;
@@ -23,9 +25,9 @@ public class GenerarEnemigos : MonoBehaviour
 
     void Generar()
     {
-        Vector3 spawnPosition = new Vector3(1.15f, 0,0);
+        Vector3 spawnPosition = new Vector3(0, 0,0);
 
-        spawnPosition = new Vector3(1.15f, Random.Range(yGeneratorUp.position.y, yGeneratorDown.position.y),0);
+        spawnPosition = new Vector3(Random.Range(xGeneratorLeft.position.x, xGeneratorRight.position.x), Random.Range(yGeneratorUp.position.y, yGeneratorDown.position.y),0);
 
         Instantiate(enemigos, spawnPosition, gameObject.transform.rotation);
     }
