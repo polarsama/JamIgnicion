95a3264045bb5973aeb7ee2f0ff5b4b39f3879ed
David Rincon <andreska9506@gmail.com>
2024-04-15 23:09:24 -0500

    Update

--- Assets/Scripts/GenerarEnemigos.cs
+++ Assets/Scripts/GenerarEnemigos.cs
@@ -17,7 +17,7 @@ public class GenerarEnemigos : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         InvokeRepeating("Generar", TimeSpawn, SpawnTime);
     }
 
